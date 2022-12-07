1b6307701a2f4b70501c4a393f1fa4b9af1dd058
matovich <matovich@hotmail.com>
2022-12-06 20:16:54 -0600

    Chapter 21 done, AZ active dir

--- SuggestionAppUI/RegisterServices.cs
+++ SuggestionAppUI/RegisterServices.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 namespace SuggestionAppUI
 {
@@ -13,8 +14,9 @@ namespace SuggestionAppUI
         {
             // Add services to the container.
             builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();
+            builder.Services.AddServerSideBlazor().AddMicrosoftIdentityConsentHandler();
             builder.Services.AddMemoryCache();
+            builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
 
             builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
